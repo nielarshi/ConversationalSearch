0c2b2c218d834437168a65dc8cdb8de4d7be8513
Kumar Nielarshi <nielarshi@gmail.com>
2017-10-13 15:36:31 -0400

    added nlp feature

---
+++ service/src/main/java/com/lexisnexis/hackathon/model/search/Response.java
@@ -0,0 +1,31 @@
+package com.lexisnexis.hackathon.model.search;
+
+public class Response {
+    private String pretext;
+    private String mainText;
+    private String postText;
+
+    public String getPretext() {
+        return pretext;
+    }
+
+    public void setPretext(String pretext) {
+        this.pretext = pretext;
+    }
+
+    public String getMainText() {
+        return mainText;
+    }
+
+    public void setMainText(String mainText) {
+        this.mainText = mainText;
+    }
+
+    public String getPostText() {
+        return postText;
+    }
+
+    public void setPostText(String postText) {
+        this.postText = postText;
+    }
+}
